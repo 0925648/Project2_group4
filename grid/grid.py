795e7778c366a15cea1d68c70c3b15a103a257ec
0874475 <0874475@hr.nl>
2017-01-19 16:14:20 +0100

    rect is anders

--- grid/grid.py
+++ grid/grid.py
@@ -1,4 +1,3 @@
-
 import pygame
 
 BLACK = (0, 0, 0)
@@ -7,8 +6,10 @@ GREEN = (0, 255, 0)
 RED = (255, 0, 0)
  
 pygame.init()
- 
-size = (1280,720)
+
+width = 1280
+height = 720 
+size = (width,height)
 screen = pygame.display.set_mode(size)
  
 pygame.display.set_caption("My Game")
@@ -27,8 +28,8 @@ while not done:
     for y in range(20):
             for x in range(20):
                 rect = pygame.Rect(x*(30+5), y*(30+5), 30, 30)
-                pygame.draw.rect(screen, BLACK, rect)
-  
+                pygame.draw.rect(screen, BLACK, (570,0, 30, 30), 0) # (570,0, 30, 30), 0 -----> WAS EERST rect
+          
                                                                                  
     pygame.display.update()
     clock.tick(60)
