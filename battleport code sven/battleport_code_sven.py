ac2f70452efd896203c3d05984f6fd2a19d15989
0925648 <svenkalkman1999@gmail.com>
2017-01-23 10:30:47 +0100

    Kaartvakken en verder button

--- battleport code sven/battleport_code_sven.py
+++ battleport code sven/battleport_code_sven.py
@@ -20,12 +20,27 @@ Blue = (0,0,255)
 Red = (255,0,0)
 Green = (0,255,0)
 
-def grid(g_height, g_width, s_height, s_width,gap):
-    for y in range(g_height):
-        for x in range(g_width):
-            rect = pygame.Rect(x*(s_width+gap), y*(s_height+gap), s_height, s_width)
-            pygame.draw.rect(game_display, White, rect)
-            return grid
+def Verder():
+    game_width = 1280
+    game_height = 720
+    size = (game_width, game_height)
+    pygame.init()
+    game_display.blit(background_image, [0,0])
+    while not process_events():
+        pygame.display.update()
+        pygame.draw.rect(game_display, White,[320,100,640,500])
+        pygame.draw.rect(game_display, Black,[325,105,630,490])
+
+        largeText = pygame.font.SysFont('Bauhaus93',30)
+        TextSurf, TextRect = text_objects("Hoe werkt het? Voorbereiding: Elke speler begint met vier boten en twee kaarten. Eerst pakken beide spelers twee kaarten van basis stapel.plaatsen beide spelers omstebeurt hun boten met de achterkant tegen eigen haven. We zijn nu klaar om te beginnen. Aanvallen en verdedigen: Tijdens het spelen van Battleport, kunnen de schepen in twee verschillende posities staan, aanvalspositie en verdedigingspositie. Aanvallend is simpel gezien de standaardpositie. Als je schip in deze positie staat, kan je normaal bewegen en aanvallen. Verdedigend maakt je schip geschikter om te verdedigen. Zo krijgt een schip in verdediging +1 range en heeft het een groter aanvals veld voor zich. Maar in verdediging mag niet meer bewegen. Een verdedigend schip mag ook alleen verticaal aanvallen.", largeText)
+        TextRect.center = ((game_width/2),(game_height/2.5))
+        game_display.blit(TextSurf, TextRect)
+                                          
+        button("Menu",1120,10,150,60,White,Green,5,Menu)
+        pygame.display.flip()
+        clock.tick(60)
+
+
 def Menu():
     game_intro()
 
@@ -35,10 +50,17 @@ def instructies():
     size = (game_width, game_height)
     pygame.init()
     game_display.blit(background_image, [0,0])
+    pygame.draw.rect(game_display, White,[320,100,640,500])
+    pygame.draw.rect(game_display, Black,[325,105,630,490])
 
     while not process_events():
         pygame.display.update()
+        largeText = pygame.font.SysFont('Bauhaus93',30)
+        TextSurf, TextRect = text_objects("Wat is battleport? Battleport kan gezien worden als een mix van Zeeslag en Hearthstone." + "\n" + "Dit omdat de gameplay lijkt op dat van Zeeslag, maar kunnen de boten nu ook verplaatst worden. Ook zijn er twee decks met kaarten die jou helpen om van je tegenstander te winnen. Beide spelers hebben vier boten en een hand met kaarten. De bedoeling van dit strategische spel in om alle schepen van je tegenstander uit te schakelen. Zet tactische zetten en gebruik je kaarten slim. Doe dit beter dan je tegenstander en de winst is voor jou", largeText)
+        TextRect.center = ((game_width/2),(game_height/2.5))
+        game_display.blit(TextSurf, TextRect)
         button("Menu",1120,10,150,60,White,Green,5,Menu)
+        button("Verder",1120,650,150,60,White,Green,5,Verder)
         pygame.display.flip()
         clock.tick(60)
 
@@ -134,15 +156,11 @@ def process_events():
     return False
 
 '''
-def message_display(text):
-    largeText = pygame.font.Font('freesansbold.ttf',20)
+def message_display(text,font,size):
+    largeText = pygame.font.Font(font,size)
     TextSurf, TextRect = text_objects(text, largeText)
     TextRect.center = ((game_width/2), (game_height/2))
     game_display.blit (TextSurf, TextRect)
-
-    pygame.display.update()
-    time.sleep(2)
-    program()
 '''
 
 def program():
@@ -157,6 +175,37 @@ def program():
             for x in range(gridX):
                 rect = pygame.Rect(x * gridSize + game_width / 2 - gridX * gridSize / 2, y * gridSize + game_height / 2 - gridY * gridSize / 2, tileSize, tileSize)
                 pygame.draw.rect(game_display, White, rect)
+        mouse = pygame.mouse.get_pos()
+        click = pygame.mouse.get_pressed()
+        if x + tileSize  > click[0] > x and y + tileSize > click[1] > y:
+            if click[0] == 1 and action != None:
+                pygame.draw.rect(game_display, Blue,(x,y,tileSize,tileSize))
+
+        pygame.draw.rect(game_display, White,[900,110,130,70])
+        pygame.draw.rect(game_display, White,[900,195,130,70])
+        pygame.draw.rect(game_display, White,[900,280,130,70])
+        pygame.draw.rect(game_display, White,[900,365,130,70])
+        pygame.draw.rect(game_display, White,[900,450,130,70])
+        pygame.draw.rect(game_display, White,[900,535,130,70])
+        pygame.draw.rect(game_display, White,[245,110,130,70])
+        pygame.draw.rect(game_display, White,[245,195,130,70])
+        pygame.draw.rect(game_display, White,[245,280,130,70])
+        pygame.draw.rect(game_display, White,[245,365,130,70])
+        pygame.draw.rect(game_display, White,[245,450,130,70])
+        pygame.draw.rect(game_display, White,[245,535,130,70])
+
+        pygame.draw.rect(game_display, Black,[905,115,120,60])
+        pygame.draw.rect(game_display, Black,[905,200,120,60])
+        pygame.draw.rect(game_display, Black,[905,285,120,60])
+        pygame.draw.rect(game_display, Black,[905,370,120,60])
+        pygame.draw.rect(game_display, Black,[905,455,120,60])
+        pygame.draw.rect(game_display, Black,[905,540,120,60])
+        pygame.draw.rect(game_display, Black,[250,115,120,60])
+        pygame.draw.rect(game_display, Black,[250,200,120,60])
+        pygame.draw.rect(game_display, Black,[250,285,120,60])
+        pygame.draw.rect(game_display, Black,[250,370,120,60])
+        pygame.draw.rect(game_display, Black,[250,455,120,60])
+        pygame.draw.rect(game_display, Black,[250,540,120,60])
         button("Menu",1120,10,150,60,White,Green,5,Menu)
         pygame.display.flip()
         clock.tick(60)
